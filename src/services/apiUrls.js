bd3e7663449de64aa2be12f89938881cb1064784
Erasmo <prueba@pruebas-MacBook-Pro.local>
2020-11-08 05:52:01 -0400

    Add enviroment variables to connect to Proxy.

--- src/services/apiUrls.js
+++ src/services/apiUrls.js
@@ -2,7 +2,8 @@ export const baseURL = "";
 
 const urls = {
     search: `https://search.torre.co/people/_search/?size=3&lang=es&aggregate=false&offset=0`,
-    user: 'http://127.0.0.1:5000/user'
+    user: `${process.env.PROXY_API}/user`,
 };
+
 
 export default urls;
