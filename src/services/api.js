680d3bbe9cc687c5173f08ba99b72388989fedb9
Erasmo <prueba@pruebas-MacBook-Pro.local>
2020-11-08 06:07:00 -0400

    Remove environment variable.

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { baseURL } from './apiUrls';
 
 const api = axios.create();
 
--- src/services/apiUrls.js
+++ src/services/apiUrls.js
@@ -2,7 +2,7 @@ export const baseURL = "";
 
 const urls = {
     search: `https://search.torre.co/people/_search/?size=3&lang=es&aggregate=false&offset=0`,
-    user: `${process.env.PROXY_API}/user`,
+    user: `https://torre-test-backend.herokuapp.com/user`,
 };
 
 
